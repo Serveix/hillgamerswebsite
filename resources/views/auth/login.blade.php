61f95937a63804da68be10042880303742a5f54e
Giancarlos Gza <giancarlosgza@gmail.com>
2019-08-27 19:53:18 -0500

    css update
    todo: green and blue cuz maincra

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,28 +1,32 @@
 @extends('layouts.default')
 @section('title', 'Iniciar sesión')
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col text-center">
-                <form action="{{ route('login') }}" method="POST">
-                    @csrf
-                    <div class="form-group">
-                        <label for="username" id="username">
-                            Username
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-12 col-md-8">
+            <div class="card">
+                <div class="card-body">
+                    <form action="{{ route('login') }}" method="POST" class="form-row">
+                        @csrf
+                        <div class="form-group col-8">
+                            <label for="username" id="username">
+                                Username
+                            </label>
                             <input type="text" name="username" class="form-control">
-                        </label>
-                    </div>
-                    <div class="form-group">
-                        <label for="password" id="password">
-                            Contraseñas
+                        </div>
+                        <div class="form-group col-8">
+                            <label for="password" id="password">
+                                Contraseñas
+                            </label>
                             <input type="password" name="password" class="form-control">
-                        </label>
-                    </div>
-                    <div class="form-group">
-                        <button class="btn btn-primary" type="submit">Iniciar Sesi&oacute;n</button>
-                    </div>
-                </form>
+                        </div>
+                        <div class="form-group col-8">
+                            <button class="btn btn-primary" type="submit">Iniciar Sesi&oacute;n</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
+</div>
 @endsection
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,34 +1,47 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
-	<title>HillGamers: @yield('title')</title>
-	<meta name="description" content="¡Servidor de Minecraft HillGamers con survival, residencias, parcelas y más!">
-	<meta name="keywords" content="Team Hill, Hill gamers, minecraft, server, survival">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta charset="utf-8">
-	<meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>HillGamers: @yield('title')</title>
+    <meta name="description" content="¡Servidor de Minecraft HillGamers con survival, residencias, parcelas y más!">
+    <meta name="keywords" content="Team Hill, Hill gamers, minecraft, server, survival">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- Google Font -->
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Pacifico|Roboto:400,500,700|Poppins:500&display=swap" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- Font Awesome -->
+    <link rel="preload" as="style" onload="this.rel='stylesheet'"
+        href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/staff.css">
+    @yield('styles')
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script>
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
 
-	@yield('styles')
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-	<script>
-		$.ajaxSetup({
-			headers: {
-				'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-			}
-		});
-	</script>
+    </script>
 </head>
-<body>
-<div class="container">
-	@include('includes.navbar')
-</div>
 
-@yield('content')
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-	@yield('scripts')
+<body>
+    @include('includes.navbar')
+        @yield('content')
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+    @yield('scripts')
 </body>
+
 </html>
