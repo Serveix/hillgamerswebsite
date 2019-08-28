baeb870f0cbee3cb08897e5caf8b6d848eb85993
Giancarlos Garza <giancarlosgza@gmail.com>
2019-08-27 23:29:17 -0500

    front end done!

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,24 +3,28 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-12 col-md-8">
+        <div class="col-12 col-md-6">
             <div class="card">
                 <div class="card-body">
+                    <div class="col-12 text-center mb-5">
+                        <h3 class="font-weight-bold text-poppins">Iniciar sesión</h3>
+                        <h5>Ingresa con tu cuenta del servidor del juego</h5>
+                    </div>
                     <form action="{{ route('login') }}" method="POST" class="form-row">
                         @csrf
-                        <div class="form-group col-8">
-                            <label for="username" id="username">
-                                Username
+                        <div class="form-group col-12">
+                            <label for="username" id="username" class="font-weight-bold">
+                                Nombre de usuario
                             </label>
                             <input type="text" name="username" class="form-control">
                         </div>
-                        <div class="form-group col-8">
-                            <label for="password" id="password">
-                                Contraseñas
+                        <div class="form-group col-12">
+                            <label for="password" id="password" class="font-weight-bold">
+                                Contraseña
                             </label>
                             <input type="password" name="password" class="form-control">
                         </div>
-                        <div class="form-group col-8">
+                        <div class="form-group col-12 text-right">
                             <button class="btn btn-primary" type="submit">Iniciar Sesi&oacute;n</button>
                         </div>
                     </form>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,17 +4,42 @@
 <link rel="stylesheet" href="css/home.css">
 @endsection
 @section('content')
+<section class="section-welcome-background">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-poppins font-weight-bold">Hillgamers</h1>
+            </div>
+            <div class="col-12">
+                <h2>¡Servidor de Minecraft HillGamers con survival, residencias, <br> parcelas y más!</h2>
+                <br>
+                <a href="/vip" class="btn btn-secondary-light">Vuelvete VIP</a>
+                @guest
+                <a href="/login" class="btn">¡Empieza ahora!</a>
+                @else 
+                <a href="/profile" class="btn">Bienvenido</a>
+                @endguest
+            </div>
+        </div>
+    </div>
+</section><br>
 <div class="container">
-	<div class="row">
-		<div class="col text-center">
-			Hay
-			<span data-playercounter-ip="mc.hillgamers.com:39535">
-				0
-			</span>
-			jugadores en <span>mc.hillgamers.com:39535</span>
-		</div>
-	</div>
+    <div class="row">
+        <div class="col-12 text-center">
+            <div class="card">
+                <div class="card-body">
+                    <h2 >
+                        Hay
+                        <span class="font-weight-bold" data-playercounter-ip="mc.hillgamers.com:39535">
+                            0
+                        </span>
+                        jugadores en: <br> <span class="text-poppins">mc.hillgamers.com:39535</span>
+                    </h2>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
-	<script src="js/main.js" type="text/javascript"></script>
-	<script src="https://leonardosnt.github.io/mc-player-counter/dist/mc-player-counter.min.js"></script>
+<script src="js/main.js" type="text/javascript"></script>
+<script src="https://leonardosnt.github.io/mc-player-counter/dist/mc-player-counter.min.js"></script>
 @endsection
